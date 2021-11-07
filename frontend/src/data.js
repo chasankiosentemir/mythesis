8871fa8d088f0b8dfdd64fea77f17b24239dcc84
chasan <chasankiosentemir@gmail.com>
2021-11-07 05:34:23 +0200

    My Thesis

---
+++ frontend/src/data.js
@@ -0,0 +1,77 @@
+const data = {
+    products: [{
+        _id: '1',
+        name: 'Dolce and Gabbana Trouser',
+        category: 'Trouser',
+        image: '/images/product-1.jpg',
+        price: 120,
+        brand: 'Dolce and Gabbana',
+        rating: 4.5,
+        numReviews: 10,
+        description:'100% Cotton'
+
+    }, 
+    {
+        _id: '2',
+        name: 'Dolce and Gabbana Trouser',
+        category: 'Trouser',
+        image: '/images/product-2.jpg',
+        price: 120,
+        brand: 'Dolce and Gabbana',
+        rating: 4.5,
+        numReviews: 10,
+        description:'100% Cotton'
+
+    }, 
+    {
+        _id: '3',
+        name: 'Dolce and Gabbana Trouser',
+        category: 'Trouser',
+        image: '/images/product-3.jpg',
+        price: 120,
+        brand: 'Dolce and Gabbana',
+        rating: 4.5,
+        numReviews: 10,
+        description:'100% Cotton'
+
+    }, 
+    {
+        _id: '4',
+        name: 'Nike Trouser',
+        category: 'Trouser',
+        image: '/images/product-4.jpg',
+        price: 120,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+        description:'100% Cotton'
+
+    }, 
+    {
+        _id: '5',
+        name: 'Trussardi Trouser',
+        category: 'Shirt',
+        image: '/images/product-5.jpg',
+        price: 120,
+        brand: 'Dolce and Gabbana',
+        rating: 4.5,
+        numReviews: 1001,
+        description:'100% Cotton'
+
+    }, 
+    {
+        _id: '6',
+        name: 'Nike Trouser',
+        category: 'Trouser',
+        image: '/images/product-6.jpg',
+        price: 150,
+        brand: 'Dolce and Gabbana',
+        rating: 4.5,
+        numReviews: 15,
+        description:'100% Cotton'
+
+    }, 
+   ],
+};
+
+export default data;
