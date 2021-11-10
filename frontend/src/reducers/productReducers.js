7535650681a3a0e07102bffaf9f6c5a3adc1ce11
chasan <chasankiosentemir@gmail.com>
2021-11-10 07:16:32 +0200

    Update code

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,6 +1,6 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constant/productConstant";
+import { PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constant/productConstants";
 
-export const productListReducer = (state = {products: []}, action) => {
+export const productListReducer = (state = {loading: true, products: []}, action) => {
     switch(action.type){
         case PRODUCT_LIST_REQUEST:
             return {loading: true};
@@ -12,5 +12,20 @@ export const productListReducer = (state = {products: []}, action) => {
                     return {loading: false, error: action.payload};
             default:
                 return state;
+    }
+}
+
+export const productDetailsReducer = (state = {product:{}, loadin: true,}, action) =>{
+    switch(action.type){
+        case PRODUCT_DETAILS_REQUEST:
+            return {loading: true};
+
+        case PRODUCT_DETAILS_SUCCESS:
+            return {loading: false, product: action.payload};
+
+        case PRODUCT_DETAILS_FAIL:
+            return {loading: false, error: action.payload};
+        default:
+            return state;    
     }
 }
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { detailsProduct } from '../actions/productActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 import Rating from '../components/Rating';
-import data from '../data';
 
 export default function ProductScreen(props) {
-    const product = data.products.find(x => x._id === props.match.params.id);
-    if(!product){
-        return <div>Product Not Found!</div>
-    }
+    const dispatch = useDispatch();
+    const productId = props.match.params.id;
+    const productDetails = useSelector((state)=> state.productDetails);
+    const {loading,error,product} = productDetails;
+
+    useEffect(() =>{
+        dispatch(detailsProduct(productId));
+    }, [dispatch, productId]);
     return (
         <div>
+            {loading ? (
+                <div className="loading-spin"> 
+                    <LoadingBox></LoadingBox>
+                </div>
+          
+            ): error ?  ( 
+            <MessageBox variant="danger">{error}</MessageBox>
+            ) : (
+            <div>
             <div className="row top">
                 <div className="col-2">
                     <img className="large" src={product.image} alt={product.name}></img>
@@ -43,6 +59,11 @@ export default function ProductScreen(props) {
                 </div>
             </div>
         </div>
+            )}
+          
+         </div>
+
+        
     )
 }
 
